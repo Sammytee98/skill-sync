ce5607a6ca8a3af1ac500e0c41ad09a69ec0a9d5
Big Sam <stosin525@gmail.com>
2025-07-04 12:20:36 +0100

    fix: change the localStorage implementation of theme store to zustand persist localStorage

--- src/components/ui/ThemeToggle.jsx
+++ src/components/ui/ThemeToggle.jsx
@@ -1,17 +1,19 @@
 import useThemeStore from "../../store/useThemeStore";
 import { FaSun, FaMoon } from "react-icons/fa6";
 import { motion } from "framer-motion";
-import { memo } from "react";
+import { memo, useCallback, useMemo } from "react";
 
 const ThemeToggle = () => {
   const { theme, toggleTheme } = useThemeStore();
 
   const FaIcon = theme === "light" ? FaMoon : FaSun;
 
+  const handleThemeToggle = useCallback(() => toggleTheme(), [toggleTheme]);
+
   return (
     <button
       type="button"
-      onClick={toggleTheme}
+      onClick={handleThemeToggle}
       className="p-1 rounded-full bg-[rgb(var(--color-border))] shadow-md cursor-pointer"
     >
       <motion.span
